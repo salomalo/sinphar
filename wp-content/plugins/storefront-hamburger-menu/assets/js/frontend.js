3a4c0c693be917faa56f5fbfae293f1e45aa5976
wendy44919 <wendy44919@gmail.com>
2017-08-07 18:35:05 +0800

    安裝套件：手機板左側 menu 顯示

---
+++ wp-content/plugins/storefront-hamburger-menu/assets/js/frontend.js
@@ -0,0 +1,24 @@
+( function( $ ) {
+	$( function() {
+		$( '.main-navigation .handheld-navigation, .main-navigation div.menu' ).prepend( '<span class="shm-close">' + shm_i18n.close + '</span>' );
+		$( '.shm-close' ).on( 'click', function() {
+			$( '.menu-toggle' ).trigger( 'click' );
+		} );
+		$( '.handheld-navigation' ).after( '<div class="bg-nav"></div>' );
+
+		$('.bg-nav').click(function(){
+			$( '.menu-toggle' ).trigger( 'click' );
+		});
+		$( document ).click( function( event ) {
+			var menuContainer = $( '.main-navigation' );
+
+			if ( $( '.main-navigation' ).hasClass( 'toggled' ) ) {
+				if ( ! menuContainer.is( event.target ) && 0 === menuContainer.has( event.target ).length ) {
+					event.preventDefault();
+					event.stopPropagation();
+					$( '.menu-toggle' ).trigger( 'click' );
+				}
+			}
+		} );
+	} );
+} )( jQuery );

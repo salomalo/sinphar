00a3d5c0f05e77087731ce2d079c9cd95e5be585
wendy44919 <wendy44919@gmail.com>
2017-08-14 08:39:19 +0800

    更新 login template

--- wp-content/themes/storefront/404.php
+++ wp-content/themes/storefront/404.php
@@ -16,10 +16,13 @@ get_header(); ?>
 				<div class="page-content">
 
 					<header class="page-header">
-						<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'storefront' ); ?></h1>
+						<h1 class="page-title">
+							<!-- <?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'storefront' ); ?> -->
+							找不到此頁面
+							</h1>
 					</header><!-- .page-header -->
 
-					<p><?php esc_html_e( 'Nothing was found at this location. Try searching, or check out the links below.', 'storefront' ); ?></p>
+					<!-- <p><?php esc_html_e( 'Nothing was found at this location. Try searching, or check out the links below.', 'storefront' ); ?></p> -->
 
 					<?php
 					echo '<section aria-label="' . esc_html__( 'Search', 'storefront' ) . '">';
@@ -32,39 +35,39 @@ get_header(); ?>
 
 					echo '</section>';
 
-					if ( storefront_is_woocommerce_activated() ) {
+					// if ( storefront_is_woocommerce_activated() ) {
 
-						echo '<div class="fourohfour-columns-2">';
+					// 	echo '<div class="fourohfour-columns-2">';
 
-							echo '<section class="col-1" aria-label="' . esc_html__( 'Promoted Products', 'storefront' ) . '">';
+					// 		echo '<section class="col-1" aria-label="' . esc_html__( 'Promoted Products', 'storefront' ) . '">';
 
-								storefront_promoted_products();
+					// 			storefront_promoted_products();
 
-							echo '</section>';
+					// 		echo '</section>';
 
-							echo '<nav class="col-2" aria-label="' . esc_html__( 'Product Categories', 'storefront' ) . '">';
+					// 		echo '<nav class="col-2" aria-label="' . esc_html__( 'Product Categories', 'storefront' ) . '">';
 
-								echo '<h2>' . esc_html__( 'Product Categories', 'storefront' ) . '</h2>';
+					// 			echo '<h2>' . esc_html__( 'Product Categories', 'storefront' ) . '</h2>';
 
-								the_widget( 'WC_Widget_Product_Categories', array(
-									'count' => 1,
-								) );
+					// 			the_widget( 'WC_Widget_Product_Categories', array(
+					// 				'count' => 1,
+					// 			) );
 
-							echo '</nav>';
+					// 		echo '</nav>';
 
-						echo '</div>';
+					// 	echo '</div>';
 
-						echo '<section aria-label="' . esc_html__( 'Popular Products', 'storefront' ) . '">';
+					// 	echo '<section aria-label="' . esc_html__( 'Popular Products', 'storefront' ) . '">';
 
-							echo '<h2>' . esc_html__( 'Popular Products', 'storefront' ) . '</h2>';
+					// 		echo '<h2>' . esc_html__( 'Popular Products', 'storefront' ) . '</h2>';
 
-							echo storefront_do_shortcode( 'best_selling_products', array(
-								'per_page' => 4,
-								'columns'  => 4,
-							) );
+					// 		echo storefront_do_shortcode( 'best_selling_products', array(
+					// 			'per_page' => 4,
+					// 			'columns'  => 4,
+					// 		) );
 
-						echo '</section>';
-					}
+					// 	echo '</section>';
+					// }
 					?>
 
 				</div><!-- .page-content -->

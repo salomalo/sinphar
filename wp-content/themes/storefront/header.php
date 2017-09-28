821866b1365b4e63c27de1c374f1cba373a3e599
wendy44919 <wendy44919@gmail.com>
2017-09-28 20:19:15 +0800

    安裝 GATC script

--- wp-content/themes/storefront/header.php
+++ wp-content/themes/storefront/header.php
@@ -19,6 +19,17 @@
 
 <!-- theme-update.css -->
 <link rel="stylesheet" href="<?php echo get_template_directory_uri().'/assets/theme-update.css?v='.rand(); ?>">
+
+<!-- GATC -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-107155896-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
 </head>
 
 <body <?php body_class(); ?>>
